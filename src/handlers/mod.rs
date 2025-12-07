544fba38556e0fcaf8aa83c255739db3b42a1280
wintun-devop <wintun.hlaing@outlook.com>
2025-12-07 12:17:06 +0630

    add base path

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -2,11 +2,12 @@ use actix_web::web;
 
 pub mod health_check;
 pub mod items;
+use crate::config;
 
 
 pub fn handler_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/api/v1")
+        web::scope(&config::config().api_base_path)
             .service(health_check::health_check)
             .configure(items::items_route_configure)
     );
